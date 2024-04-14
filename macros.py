85a0d43d318e971152e77c47f56f42d6f0e2b4d7
Maciej Maczka <99673545+123Maciek@users.noreply.github.com>
2024-04-14 12:21:20 +0200

    Macro pt.3
    Macro Window finished

--- macros.py
+++ macros.py
@@ -6,6 +6,106 @@ import sys
 from tkinter import font
 import ctypes
 from pynput import mouse, keyboard
+from tkinter import messagebox
+import os
+
+def save_macro():
+    global chk_var
+    if chk_var.get() != 0:
+        messagebox.showerror("Hide mouse move error", "To run your macro you have to disavle hide mouse move option.")
+        return
+    
+    try:
+        act = text_to_action(tbValue.get("1.0", tk.END))
+    except:
+        messagebox.showerror("Recording error", "Incorrect recording value.")
+        return
+    
+    with open('name.txt', 'r') as file:
+        name = file.readline()
+        loc = file.readline()
+    projectLoc = loc
+    name = name.replace('\n', '')
+    if projectLoc == None or projectLoc == "":
+        messagebox.showerror("Project error", "It seams that ur project doesn't exists.")
+        return
+    
+    test_act = tbValue.get("1.0", tk.END).replace("\n", "")
+    test_act = test_act.replace(" ", "")
+    if test_act == "":
+        messagebox.showerror("Recording error", "You cannot save empty recording.")
+        return
+
+    macro_loc = projectLoc[:-(len(name) + 4)]
+    macro_name = tbName.get()
+    if macro_name == "" or macro_name == None:
+        messagebox.showerror("Macro Name error", "Your macro name cannot be empty.")
+        return
+    new_macro_name = macro_name.replace(" ", "")
+    new_macro_name = new_macro_name.replace(".", "")
+    new_macro_name = new_macro_name.replace("\n", "")
+    if new_macro_name != macro_name:
+        messagebox.showerror("Macro Name error", "Incorrect macro name value. Macro's name cannot contain space and dot")
+        return
+    
+    macro_loc += f"Macros\\{macro_name}.txt"
+    if os.path.isfile(macro_loc):
+        result = messagebox.askquestion("Confirmation", "Macro of this name already exist in your current project. Do you wanna replace it?")
+        if result.lower() == 'yes':
+            os.remove(macro_loc)
+        else:
+            return
+    with open(macro_loc, "w") as file:
+        file.write(tbValue.get("1.0", tk.END))
+    
+    root.destroy()
+            
+
+def play_current_action():
+    global chk_var
+    if chk_var.get() != 0:
+        messagebox.showerror("Hide mouse move error", "To run your macro you have to disavle hide mouse move option.")
+        return
+
+    act = text_to_action(tbValue.get("1.0", tk.END))
+    time.sleep(3)
+    play_actions(act)
+
+def text_to_action(text):
+    lines = text.split("\n")
+    act = []
+    for line in lines:
+        if line != "":
+            line = line[:-1]
+            line = line[1:]
+            line = line.replace(" ", "")
+            args = line.split(",")
+            arr = []
+            for arg in args:
+                if arg[0] == "'" and arg[len(arg)-1] == "'":
+                    arg = arg[1:]
+                    arg = arg[:-1]
+                else:
+                    try:
+                        arg = int(arg)
+                    except:
+                        arg = float(arg)
+                arr.append(arg)
+            act.append(arr)
+    return act
+
+def action_to_text(arr):
+    result = ""
+    num = 0
+    for el in arr:
+        string = str(el)
+        string = string.replace("[", "(")
+        string = string.replace("]", ")")
+        result += string
+        if num != len(arr)-1:
+            result += "\n"
+        num += 1
+    return result
 
 def start_record():
     # Start listeners
@@ -25,11 +125,13 @@ def stop_recording():
     #mouse_listener.stop()
     #keyboard_listener.stop()
     global is_recording
+    global actions
     is_recording = False
     btnStart.config(text="START - F8", fg="white", bg="green", activeforeground="white", activebackground="darkgreen")
-    time.sleep(3)
-    on_checkbox_click()
-    play_actions(actions)
+    messagebox.showinfo("Succesfull recording", "Your macro has been succesfully recorded.")
+    set_tbValue(action_to_text(actions))
+    chk_var.set(0)
+    
 
 def on_window_close():
     mouse_listener.stop()
@@ -47,24 +149,72 @@ def remove_prefix_if_matches(string, prefix):
 
 def actions_without_movement():
     result = ""
-    new_act = hide_chain_values(actions)
-    for act in new_act:
-        result += str(act)
-        result += "\n"
+    new_act = hide_chain_values(text_to_action(tbValue.get("1.0", tk.END)))
+    result = action_to_text(new_act)
     return result
 
+def number_in_array(arr, el):
+    num = 0
+    el = str(el)
+    el = el.replace("[", "(")
+    el = el.replace("]", ")")
+    for a in arr:
+        if str(a) == el:
+            return num
+        num += 1
+    return num
+
+def number_in_array2(arr, el):
+    num = 0
+    el = str(el)
+    for a in arr:
+        if str(a) == el:
+            return num
+        num += 1
+    return num
+
+def insert_value(arr, value, index):
+    if index < 0 or index > len(arr):
+        arr.append(value)
+        return arr
+    return arr[:index] + [value] + arr[index:]
+
 def actions_with_movement():
+    global actions
     result = ""
-    for act in actions:
-        result += str(act)
-        result += "\n"
+    cur_act = text_to_action(tbValue.get("1.0", tk.END))
+    mouse_moves_number = []
+    num = 0
+    for el in cur_act:
+        if el[0] == "mouse_move":
+            mouse_moves_number.append(el)
+        num += 1
+    for object in mouse_moves_number:
+        number = number_in_array2(cur_act, object)
+        mm = number_in_array(actions, cur_act[number])
+        next = 0
+        if (number + 1) == len(cur_act):
+            next = len(actions) - 1
+        else:
+            next = number_in_array(actions, cur_act[number+1])
+        iteration = 1
+        range_list = range(next-mm-1)
+        result_list = [num + mm + 1 for num in range_list]
+        for i in result_list:
+            if (actions[i])[0] != "mouse_move":
+                break
+            cur_act = insert_value(cur_act, actions[i], number + iteration)
+            iteration += 1
+    actions = []
+    for i in cur_act:
+        actions.append(tuple(i))
+    result = action_to_text(cur_act)
     return result
 
 def remove_element(arr, elem):
     return [x for x in arr if x != elem]
 
 def play_actions(act):
-    print("Start playing actions")
     start = time.time()
 
     for action in act:
@@ -81,7 +231,6 @@ def play_actions(act):
             pyautogui.moveTo(x, y)
         elif action_type == "mouse_press":
             x, y, button = params
-            print(button)
             pyautogui.mouseDown(x, y, button)
         elif action_type == "mouse_release":
             x, y, button = params
@@ -93,13 +242,19 @@ def play_actions(act):
             key = params[0]
             pyautogui.keyUp(key)
 
+def set_tbValue(text):
+    tbValue.delete(1.0, tk.END)
+    tbValue.insert(tk.END, text)
+
 def on_checkbox_click():
     if chk_var.get() == 1:
+        txt = actions_without_movement()
         tbValue.delete(1.0, tk.END)
-        tbValue.insert(tk.END, actions_without_movement())
+        tbValue.insert(tk.END, txt)
     else:
+        txt = actions_with_movement()
         tbValue.delete(1.0, tk.END)
-        tbValue.insert(tk.END, actions_with_movement())
+        tbValue.insert(tk.END, txt)
         
 
 # Create the main window
@@ -133,8 +288,12 @@ checkbox = tk.Checkbutton(root, text="Hide mouse movement", variable=chk_var, co
 checkbox.pack(pady=20)
 tbValue = tk.Text(root, width=70, height=20, font=font.Font())
 tbValue.pack(side=tk.TOP)
+btnSave = tk.Button(root, text="SAVE", fg="white", bg="green", activeforeground="white", activebackground="darkgreen", bd=1, relief=tk.FLAT, font=("Heltevica", 25), command=save_macro)
+btnSave.pack(side=tk.LEFT, pady=(0, 50), padx=(120, 20))
 btnStart = tk.Button(root, text="START - F8", fg="white", bg="green", activeforeground="white", activebackground="darkgreen", bd=1, relief=tk.FLAT, font=("Heltevica", 25))
-btnStart.pack(side=tk.BOTTOM, pady=(0, 50))
+btnStart.pack(side=tk.LEFT, pady=(0, 50), padx=10)
+btnPlay = tk.Button(root, text="PLAY - 3 SEC DELAY", fg="white", bg="green", activeforeground="white", activebackground="darkgreen", bd=1, relief=tk.FLAT, font=("Heltevica", 25), command=play_current_action)
+btnPlay.pack(side=tk.LEFT, pady=(0, 50), padx=(20, 0))
 
 # Lists to store recorded actions
 actions = []
@@ -180,22 +339,21 @@ def on_mouse_click(x, y, button, pressed):
 def on_key_press(key):
     if is_recording == False:
         return
-
     elapsed_time = time.time() - start_time
     try:
         key_char = key.char
     except AttributeError:
         key_char = str(key)
 
-
     key_char = key_char.lower()
     key_char = remove_prefix_if_matches(key_char, "key.")
     if key_char == "cmd":
         key_char = "win"
+    if key_char == "alt_l":
+        key_char = "alt"
 
     if key_char == "f8":
         return
-
     actions.append(("key_press", key_char, elapsed_time))
 
 def on_key_release(key):
@@ -215,10 +373,15 @@ def on_key_release(key):
             start_record()
             return
 
+    if is_recording == False:
+        return
+
     key_char = key_char.lower()
     key_char = remove_prefix_if_matches(key_char, "key.")
     if key_char == "cmd":
         key_char = "win"
+    if key_char == "alt_l":
+        key_char = "alt"
 
     actions.append(("key_release", key_char, elapsed_time))
 
--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import pyautogui
 
 
-pyautogui.mouseDown('1', 100, 100)
-pyautogui.mouseUp('1', 100, 100)
+def insert_value(arr, value, index):
+    if index < 0 or index > len(arr):
+        print(f"Invalid index {index}. Appending {value} to the end.")
+        arr.append(value)
+        return arr
+    
+    return arr[:index] + [value] + arr[index:]
+    
+a = [1, 2, 3, 4, 5, 6]
+print(insert_value(a, 7, 2))
