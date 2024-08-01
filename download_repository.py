ed34318ab403158e4eecf81ff022e6b2c229e757
Maciej Maczka <99673545+123Maciek@users.noreply.github.com>
2024-08-01 17:39:32 +0200

    Changing some issues

--- download_repository.py
+++ download_repository.py
@@ -22,7 +22,7 @@ def delete_files_except_script(directory):
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         try:
-            if os.path.isfile(file_path) and filename not in ["name.txt", "projects.txt", "start.bat", "window.vbs"]:
+            if os.path.isfile(file_path) and filename not in ["name.txt", "projects.txt", "start.bat", "window.vbs", "download_repository.py"]:
                 os.remove(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
